cc146affde5fd7b65c63e0134f475291c87592e0
tizianocoppoli <tiziano.coppoli@gmail.com>
2021-04-20 10:19:49 +0000

    setup development environment

--- todo/test_views.py
+++ todo/test_views.py
@@ -11,7 +11,7 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'todo/todo_list.html')
 
     def test_get_add_item_page(self):
-        response = self.client.get('/additem')
+        response = self.client.get('/add/')
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'todo/add_item.html')
 
@@ -22,7 +22,7 @@ class TestViews(TestCase):
         self.assertTemplateUsed(response, 'todo/edit_item.html')
 
     def test_can_add_item(self):
-        response = self.client.post('/additem', {'name': 'Test Added Item'})
+        response = self.client.post('/add/', {'name': 'Test Added Item'})
         self.assertRedirects(response, '/')
 
     def test_can_delete_item(self):
